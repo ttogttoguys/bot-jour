4b9583d2c73f83130ce6407ec02f781c84c60915
kruch <kryuchkov2001@mail.ru>
2022-09-10 22:05:53 +0300

    add:get_student_mainMenu

--- utils/markups/markups.py
+++ utils/markups/markups.py
@@ -12,3 +12,23 @@ def get_student_mainMenu() -> ReplyKeyboardMarkup:
     ReplyKeyboardMarkup_student = InlineKeyboardMarkup().add(button_schedule, button_tasks)
 
     return ReplyKeyboardMarkup_student
+
+
+def get_professor_mainMenu() -> ReplyKeyboardMarkup:
+    """
+    Возвращает основную клавиатуру, с клавишами:
+        Добавить задание, Просмотреть добавленные задания
+    """
+    button_addTask = InlineKeyboardButton(text='Добавить задание')
+    button_viewTasks = InlineKeyboardButton(text='Просмотреть добавленные задания')
+    ReplyKeyboardMarkup_professor = InlineKeyboardMarkup().add(button_addTask, button_viewTasks)
+
+    return ReplyKeyboardMarkup_professor
+
+
+def get_ADMIN_mainMenu() -> ReplyKeyboardMarkup:
+    """
+        Возвращает основную клавиатуру, с клавишами:
+
+        """
+    pass
