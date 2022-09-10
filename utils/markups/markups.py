e00b3da487939c56e7566142259df139871ab390
kruch <kryuchkov2001@mail.ru>
2022-09-10 22:25:36 +0300

    add:KeyboardMarkups

--- utils/markups/markups.py
+++ utils/markups/markups.py
@@ -2,6 +2,19 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
 
+def get_mainMenu() -> ReplyKeyboardMarkup:
+    """
+    Возвращает основную клавиатуру, с клавишами:
+        Студент, Учитель, Админ
+    """
+    button_student = InlineKeyboardButton(text='Студент')
+    button_professor = InlineKeyboardButton(text='Учитель')
+    button_ADMIN = InlineKeyboardButton(text='Админ')
+    ReplyKeyboardMarkup_main = InlineKeyboardMarkup().add(button_student, button_professor, button_ADMIN)
+
+    return ReplyKeyboardMarkup_main
+
+
 def get_student_mainMenu() -> ReplyKeyboardMarkup:
     """
     Возвращает основную клавиатуру, с клавишами:
@@ -9,9 +22,23 @@ def get_student_mainMenu() -> ReplyKeyboardMarkup:
     """
     button_schedule = InlineKeyboardButton(text='Расписание')
     button_tasks = InlineKeyboardButton(text='Задания')
-    ReplyKeyboardMarkup_student = InlineKeyboardMarkup().add(button_schedule, button_tasks)
+    ReplyKeyboardMarkup_student_1 = InlineKeyboardMarkup().add(button_schedule, button_tasks)
 
-    return ReplyKeyboardMarkup_student
+    return ReplyKeyboardMarkup_student_1
+
+
+def get_student_mainMenu() -> ReplyKeyboardMarkup:
+    """
+    Возвращает основную клавиатуру, с клавишами:
+        Расписание, Задание
+    """
+    button_NextClass = InlineKeyboardButton(text='Следующее занятие')
+    button_Tommorow = InlineKeyboardButton(text='Завтра')
+    button_Week = InlineKeyboardButton(text='Неделя')
+    button_All = InlineKeyboardButton(text='Все')
+    ReplyKeyboardMarkup_student_2 = InlineKeyboardMarkup().add(button_NextClass, button_Tommorow, button_Week, button_All)
+
+    return ReplyKeyboardMarkup_student_2
 
 
 def get_professor_mainMenu() -> ReplyKeyboardMarkup:
@@ -21,9 +48,9 @@ def get_professor_mainMenu() -> ReplyKeyboardMarkup:
     """
     button_addTask = InlineKeyboardButton(text='Добавить задание')
     button_viewTasks = InlineKeyboardButton(text='Просмотреть добавленные задания')
-    ReplyKeyboardMarkup_professor = InlineKeyboardMarkup().add(button_addTask, button_viewTasks)
+    ReplyKeyboardMarkup_professor_1 = InlineKeyboardMarkup().add(button_addTask, button_viewTasks)
 
-    return ReplyKeyboardMarkup_professor
+    return ReplyKeyboardMarkup_professor_1
 
 
 def get_ADMIN_mainMenu() -> ReplyKeyboardMarkup:
