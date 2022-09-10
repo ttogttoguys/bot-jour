923d2bad9fd34a88aa68d5a0bfa9b282f5e49c37
yakov <yakovglee@gmail.com>
2022-09-10 19:05:00 +0300

    add: main menu

---
+++ utils/markups/markups.py
@@ -0,0 +1,10 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+
+
+def get_mainMenu() -> ReplyKeyboardMarkup:
+    """
+    Возвращает основную клавиатуру, с клавишами:
+        Расписание, Задание
+    """
+    pass
